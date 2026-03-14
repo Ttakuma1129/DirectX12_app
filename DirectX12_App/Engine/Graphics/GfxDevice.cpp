7ce91f1c49da5d9ce8a52edc801a1eb85b3386c4
takuma <kss23200080@nsgcl.ocm>
2026-03-14 22:37:02 +0900

    InitializeFrameResourcesに頂点バッファ作成を追加

--- DirectX12_App/Engine/Graphics/GfxDevice.cpp
+++ DirectX12_App/Engine/Graphics/GfxDevice.cpp
@@ -191,6 +191,30 @@ bool GfxDevice::InitializeFrameResources() {
 		return false;
 	}
 
+	// 頂点バッファ作成
+	D3D12_HEAP_PROPERTIES heapProps = {};
+	heapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeof(Vertex) * 3;
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.Format = DXGI_FORMAT_UNKNOWN;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	hr = m_device->CreateCommittedResource(
+		&heapProps,
+		D3D12_HEAP_FLAG_NONE,
+		&resDesc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&m_vertexBuffer));
+	if (FAILED(hr)) {
+		return false;
+	}
+
 	return true;
 }
 

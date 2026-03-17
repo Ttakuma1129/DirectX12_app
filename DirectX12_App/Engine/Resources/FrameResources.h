2248f5212187b9b93839cd4d1873bbe8ba6c214c
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-17 18:42:37 +0900

    定数バッファの作成を追加

--- DirectX12_App/Engine/Resources/FrameResources.h
+++ DirectX12_App/Engine/Resources/FrameResources.h
@@ -2,6 +2,11 @@
 #include <d3d12.h>
 #include <wrl/client.h>
 #include <cstdint>
+#include <DirectXMath.h>
+
+struct alignas(256) SceneConstant {
+	DirectX::XMFLOAT4X4 mvp;
+};
 
 class FrameResources{
 public:
@@ -13,8 +18,20 @@ public:
 		return m_allocator.Get();
 	}
 
+	// 外部から定数バッファを取得するためのゲッター
+	ID3D12Resource* GetConstantBuffer() {
+		return m_constantBuffer.Get();
+	}
+
+	// 外部から定数マップを取得するためのゲッター
+	SceneConstant* GetConstantMapped() {
+		return m_mapped;
+	}
+
 	uint64_t fenceValue = 0;
 
 private:
 	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_allocator; //コマンドアロケータ
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_constantBuffer; // 定数バッファ
+	SceneConstant* m_mapped = nullptr; // 常時マッピング
 };

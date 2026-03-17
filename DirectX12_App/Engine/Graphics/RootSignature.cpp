3219eab05e36fb6f4f680351384aacba95c76a8e
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-17 18:55:18 +0900

    CBVをルートパラメータとして追加

--- DirectX12_App/Engine/Graphics/RootSignature.cpp
+++ DirectX12_App/Engine/Graphics/RootSignature.cpp
@@ -1,9 +1,17 @@
 #include "RootSignature.h"
 
 bool RootSignature::Initialize(ID3D12Device* device) {
+	// CBVをルートパラメータとして追加
+	D3D12_ROOT_PARAMETER rootParam = {};
+	rootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParam.Descriptor.ShaderRegister = 0;
+	rootParam.Descriptor.RegisterSpace = 0;
+	rootParam.ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+
+	// ルートシグネチャの設定
 	D3D12_ROOT_SIGNATURE_DESC desc = {};
-	desc.NumParameters = 0;
-	desc.pParameters = nullptr;
+	desc.NumParameters = 1;
+	desc.pParameters = &rootParam;
 	desc.NumStaticSamplers = 0;
 	desc.pStaticSamplers = nullptr;
 	desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
@@ -18,7 +26,6 @@ bool RootSignature::Initialize(ID3D12Device* device) {
 		&signatureBlob,
 		&errorBlob
 	);
-
 	if (FAILED(hr)) {
 		return false;
 	}
@@ -30,9 +37,9 @@ bool RootSignature::Initialize(ID3D12Device* device) {
 		signatureBlob->GetBufferSize(),
 		IID_PPV_ARGS(&m_rootSignature)
 	);
-
 	if (FAILED(hr)) {
 		return false;
 	}
+
 	return true;
 }

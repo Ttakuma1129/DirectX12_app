fcd2b8c5c70a7b659484088275b28d63cb22a62e
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-17 19:00:11 +0900

    output.posをMVP行列で変換するように変更、SceneConstant追加

--- DirectX12_App/Shaders/VertexShader.hlsl
+++ DirectX12_App/Shaders/VertexShader.hlsl
@@ -1,20 +1,22 @@
-struct VSInput
-{
+// 定数バッファ
+cbuffer SceneConstant : register(b0){ 
+    float4x4 mvp;
+}
+
+struct VSInput{
     float3 pos : POSITION;
     float4 color : COLOR;
 };
 
-struct VSOutput
-{
+struct VSOutput{
     float4 pos : SV_POSITION;
     float4 color : COLOR;
 };
 
-VSOutput main(VSInput input)
-{
+VSOutput main(VSInput input){
     VSOutput output;
-    // float3からfloat4に変換
-    output.pos = float4(input.pos, 1.0f);
+    // MVP行列で変換
+    output.pos = mul(mvp, float4(input.pos, 1.0f));
     output.color = input.color;
     return output;
 }

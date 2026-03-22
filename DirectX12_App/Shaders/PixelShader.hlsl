99a69b369adb14a8e240393661656843d9be825f
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-23 00:39:12 +0900

    colorをuvに変更、mainのreturnされる値を変更

--- DirectX12_App/Shaders/PixelShader.hlsl
+++ DirectX12_App/Shaders/PixelShader.hlsl
@@ -1,8 +1,11 @@
+Texture2D tex : register(t0);
+SamplerState smp : register(s0);
+
 struct PSInput{
     float4 pos : SV_POSITION;
-    float4 color : COLOR;
+    float2 uv : TEXCOORD;
 };
 
 float4 main(PSInput input) : SV_TARGET{
-    return input.color;
+    return tex.Sample(smp,input.uv);
 }

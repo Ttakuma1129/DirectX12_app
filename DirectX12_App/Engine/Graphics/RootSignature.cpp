849152d05e62e8153b0b55a7ebb244a5e3a8d6ff
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-23 01:23:17 +0900

    InitializeにSRVレンジ、SRVDescriptorTable、StaticSamplerの設定を追加

--- DirectX12_App/Engine/Graphics/RootSignature.cpp
+++ DirectX12_App/Engine/Graphics/RootSignature.cpp
@@ -1,19 +1,45 @@
 #include "RootSignature.h"
 
 bool RootSignature::Initialize(ID3D12Device* device) {
+
+	// SRVレンジを設定
+	D3D12_DESCRIPTOR_RANGE srvRange = {};
+	srvRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	srvRange.NumDescriptors = 1;
+	srvRange.BaseShaderRegister = 0;
+	srvRange.RegisterSpace = 0;
+	srvRange.OffsetInDescriptorsFromTableStart = 0;
+
 	// CBVをルートパラメータとして追加
-	D3D12_ROOT_PARAMETER rootParam = {};
-	rootParam.ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
-	rootParam.Descriptor.ShaderRegister = 0;
-	rootParam.Descriptor.RegisterSpace = 0;
-	rootParam.ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+	D3D12_ROOT_PARAMETER rootParam[2] = {};
+	rootParam[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParam[0].Descriptor.ShaderRegister = 0;
+	rootParam[0].Descriptor.RegisterSpace = 0;
+	rootParam[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+
+	// SRVのDescriptorTableをルートパラメータとして追加
+	rootParam[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+	rootParam[1].DescriptorTable.NumDescriptorRanges = 1;
+	rootParam[1].DescriptorTable.pDescriptorRanges = &srvRange;
+	rootParam[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+
+	// StaticSamplerの設定
+	D3D12_STATIC_SAMPLER_DESC smpDesc = {};
+	smpDesc.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+	smpDesc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	smpDesc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	smpDesc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	smpDesc.ShaderRegister = 0;
+	smpDesc.RegisterSpace = 0;
+	smpDesc.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	smpDesc.MaxLOD = D3D12_FLOAT32_MAX;
 
 	// ルートシグネチャの設定
 	D3D12_ROOT_SIGNATURE_DESC desc = {};
-	desc.NumParameters = 1;
-	desc.pParameters = &rootParam;
-	desc.NumStaticSamplers = 0;
-	desc.pStaticSamplers = nullptr;
+	desc.NumParameters = 2;
+	desc.pParameters = rootParam;
+	desc.NumStaticSamplers = 1;
+	desc.pStaticSamplers = &smpDesc;
 	desc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
 
 	// シリアライズ

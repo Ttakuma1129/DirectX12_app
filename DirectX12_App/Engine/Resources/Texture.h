abe67efbf77e71fd1f8cb09417dda2a5569117ae
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-23 17:28:00 +0900

    Textureクラスの修正

--- DirectX12_App/Engine/Resources/Texture.h
+++ DirectX12_App/Engine/Resources/Texture.h
@@ -9,11 +9,19 @@ public:
 	bool Create(
 		ID3D12Device* device,
 		ID3D12GraphicsCommandList* cmdList,
-		uint32_t with,
+		uint32_t width,
 		uint32_t height,
 		const void* pixels,
 		D3D12_CPU_DESCRIPTOR_HANDLE srvHandle);
 
+	// 中間バッファ解放
+	void ReleaseUploadBuffer();
+
+
+	// 外部からリソースを取得するためのゲッター
+	ID3D12Resource* GetResource() const {
+		return m_texture.Get();
+	}
 private:
 	Microsoft::WRL::ComPtr<ID3D12Resource> m_texture; // GPU上のテクスチャ
 	Microsoft::WRL::ComPtr<ID3D12Resource> m_uploadBuffer; // 中間バッファ

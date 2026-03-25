7711f7b8f9c271a9bdf8ffe28472cc4f77c81840
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-26 02:17:07 +0900

    Sceneクラスを新規作成

--- DirectX12_App/App/Scene.h
+++ DirectX12_App/App/Scene.h
@@ -1,1 +1,30 @@
 #pragma once
+#include <DirectXMath.h>
+#include <chrono>
+#include <cstdint>
+
+class Scene {
+public:
+	void Initialize();
+	void Update();
+
+	DirectX::XMMATRIX GetViewMatrix() const {
+
+	}
+
+	DirectX::XMMATRIX GetProjMatrix() const {
+
+	}
+
+	uint32_t GetObjectCount() const {
+		return OBJECT_COUNT;
+	}
+
+private:
+	static constexpr uint32_t OBJECT_COUNT = 2;
+
+	float m_elapsed = 0.0f;
+	float m_aspect = 16.0f / 9.0f;
+	
+	std::chrono::high_resolution_clock::time_point m_startTime; // フレームの開始時間を保存
+};

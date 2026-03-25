617b715a33842198db18bb9f51d97687962072a2
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-25 16:43:15 +0900

    MeshClassを作成

--- DirectX12_App/Engine/Resources/Mesh.h
+++ DirectX12_App/Engine/Resources/Mesh.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <d3d12.h>
+#include <wrl/client.h>
+#include <cstdint>
+
+
+class  Mesh{
+public:
+	bool Create(ID3D12Device* device, const void* vertices, uint32_t vertexSize, uint32_t stride, const uint16_t* indices, uint32_t indexCount);
+
+	void Bind(ID3D12GraphicsCommandList* cmdList);
+
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private:
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_vertexBuffer; // 頂点バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> m_indexBuffer; // インデックスバッファ
+
+	// バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView = {};
+	D3D12_INDEX_BUFFER_VIEW m_indexBufferView = {};
+
+	uint32_t m_indexCount = 0;
+};

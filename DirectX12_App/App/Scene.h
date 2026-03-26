50c1ba201b04ad7c01e97e7a48a793e80b459e70
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-26 15:59:37 +0900

    引数と変数を変更

--- DirectX12_App/App/Scene.h
+++ DirectX12_App/App/Scene.h
@@ -5,7 +5,7 @@
 
 class Scene {
 public:
-	void Initialize();
+	void Initialize(float aspectRatio);
 	void Update();
 
 	DirectX::XMMATRIX GetViewMatrix() const {
@@ -24,7 +24,7 @@ private:
 	static constexpr uint32_t OBJECT_COUNT = 2;
 
 	float m_elapsed = 0.0f;
-	float m_aspect = 16.0f / 9.0f;
+	float m_aspect = 1.0f;
 	
 	std::chrono::high_resolution_clock::time_point m_startTime; // フレームの開始時間を保存
 };

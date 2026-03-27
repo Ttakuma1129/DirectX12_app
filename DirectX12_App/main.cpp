98d2048df50d5fc2299fab1e8527694dad0cbafa
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-27 10:57:54 +0900

    ImGuiシャットダウンを追加

--- DirectX12_App/main.cpp
+++ DirectX12_App/main.cpp
@@ -60,5 +60,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		gfxDevice.EndFrame();
 	}
+
+	// ImGuiシャットダウン
+	ImGui_ImplDX12_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
 	return 0;
 }

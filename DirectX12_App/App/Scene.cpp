4f2e2b79c4450803987cc6625742a79169a8025a
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-27 14:47:42 +0900

    作った変数を使うように変更

--- DirectX12_App/App/Scene.cpp
+++ DirectX12_App/App/Scene.cpp
@@ -16,7 +16,7 @@ void Scene::Update() {
 DirectX::XMMATRIX Scene::GetViewMatrix()const {
 	using namespace DirectX;
 	// View行列 (カメラの設定)
-	XMVECTOR eye = XMVectorSet(0.0f, 0.7f, -3.0f, 0.0f); // カメラ位置
+	XMVECTOR eye = XMVectorSet(m_cameraPos[0], m_cameraPos[1], m_cameraPos[2], 0.0f); // カメラ位置
 	XMVECTOR target = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f); // 注視点
 	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f); // 上方向
 	return XMMatrixLookAtLH(eye, target, up); // View行列
@@ -25,18 +25,18 @@ DirectX::XMMATRIX Scene::GetViewMatrix()const {
 DirectX::XMMATRIX Scene::GetProjMatrix() const {
 	using namespace DirectX;
 	// Projection行列　(透視投影)
-	float fov = XMConvertToRadians(45.0f); // 視野角
+	float fov = XMConvertToRadians(m_fov); // 視野角
 	return XMMatrixPerspectiveFovLH(fov, m_aspect, 0.1f, 100.0f);
 }
 
 DirectX::XMMATRIX Scene::GetModelMatrix(uint32_t index) const {
 	using namespace DirectX;
 	if (index == 0) {
-		return XMMatrixRotationY(m_elapsed * XM_2PI * 0.5f) * XMMatrixTranslation(-1.5f, 0.0f, 0.0f);
+		return XMMatrixRotationY(m_elapsed * XM_2PI * m_rotationSpeed) * XMMatrixTranslation(-1.5f, 0.0f, 0.0f);
 
 	}
 	else {
-		return XMMatrixRotationY(-m_elapsed * XM_2PI * 0.5f) * XMMatrixTranslation(1.5f, 0.0f, 0.0f);
+		return XMMatrixRotationY(-m_elapsed * XM_2PI * m_rotationSpeed) * XMMatrixTranslation(1.5f, 0.0f, 0.0f);
 
 	}
 }

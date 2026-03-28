ea650a8e2bfb5b02fd254a63a4ce06e527cd8104
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-28 16:42:18 +0900

    ModelLoaderクラスを作成

--- DirectX12_App/Engine/Resources/ModelLoader.h
+++ DirectX12_App/Engine/Resources/ModelLoader.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <d3d12.h>
+#include <vector>
+#include <string>
+#include <cstdint>
+
+struct ModelVertex {
+	float position[3];
+	float uv[2];
+};
+
+struct ModelData{
+	std::vector<ModelVertex> vertices;
+	std::vector<uint16_t> indices;
+};
+
+class ModelLoader {
+public:
+	// OBJファイルを読み込んで頂点・インデックスデータに変換する
+	static bool LoadOBJ(const std::string& flilepath, ModelData& outData);
+};

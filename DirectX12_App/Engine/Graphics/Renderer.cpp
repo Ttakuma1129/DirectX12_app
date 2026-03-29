24be6675f0992a5d64452f5ae4f0d44e73110d30
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-29 19:16:00 +0900

    モデルを変更

--- DirectX12_App/Engine/Graphics/Renderer.cpp
+++ DirectX12_App/Engine/Graphics/Renderer.cpp
@@ -66,7 +66,7 @@ bool Renderer::Initialize(ID3D12Device* device, ID3D12CommandQueue* commandQueue
 
 	// OBJÉÇÉfÉãì«Ç›çûÇ›
 	ModelData modelData;
-	if (!ModelLoader::LoadOBJ("App/Models/sphere.obj", modelData)) {
+	if (!ModelLoader::LoadOBJ("App/Models/sword.obj", modelData)) {
 		OutputDebugStringA("Failed to load OBJ model\n");
 		return false;
 	}

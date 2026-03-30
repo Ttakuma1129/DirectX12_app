b9effb4304533ac96370ab48aae6a6bb1db23958
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-30 23:08:50 +0900

    ライトパラメータとゲッターを追加

--- DirectX12_App/App/Scene.h
+++ DirectX12_App/App/Scene.h
@@ -34,6 +34,18 @@ public:
 		return OBJECT_COUNT;
 	}
 
+	float* GetLightDir() {
+		return m_lightDir;
+	}
+
+	float* GetLightColor() {
+		return m_lightColor;
+	}
+
+	float* GetAmbientColor() {
+		return m_ambientColor;
+	}
+
 private:
 	static constexpr uint32_t OBJECT_COUNT = 2;
 
@@ -43,6 +55,11 @@ private:
 	float m_cameraPos[3] = { 0.0f,0.7f,-3.0f };
 	float m_fov = 45.0f;
 	float m_scale = 2.0f;
+
+	// ライトパラメータ
+	float m_lightDir[3] = { -0.5f, -1.0, 0.5f };
+	float m_lightColor[3] = { 1.0f, 1.0f, 1.0f };
+	float m_ambientColor[3] = { 0.15f, 0.15f, 0.15f };
 	
 	std::chrono::high_resolution_clock::time_point m_startTime; // フレームの開始時間を保存
 };

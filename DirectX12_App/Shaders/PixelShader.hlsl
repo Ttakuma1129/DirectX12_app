24baa671d444913b79d02316d38900c1744da15c
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-03-30 22:40:37 +0900

    SceneConstantを追加

--- DirectX12_App/Shaders/PixelShader.hlsl
+++ DirectX12_App/Shaders/PixelShader.hlsl
@@ -1,3 +1,11 @@
+cbuffer SceneConstant : register(b0){
+    float4x4 view;
+    float4x4 proj;
+    float4 lightDir;
+    float4 lightColor;
+    float4 ambientColot;
+}
+
 Texture2D tex : register(t0);
 SamplerState smp : register(s0);
 

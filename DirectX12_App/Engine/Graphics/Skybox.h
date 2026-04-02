8ea01b39a1b8d6b31e3bfe524e46885b30927fae
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-02 22:59:28 +0900

    インクルードを追加

--- DirectX12_App/Engine/Graphics/Skybox.h
+++ DirectX12_App/Engine/Graphics/Skybox.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <d3d12.h>
+#include <wrl/client.h>
+#include <d3dcompiler.h>
+
+#include "DescriptorHeap.h"
+#include "../Resources//Mesh.h"
+#include "../Resources/CubeMapTexture.h"
+#include "../Resources/FrameResources.h"

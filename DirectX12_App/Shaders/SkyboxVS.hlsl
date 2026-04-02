ccca6604b60ff2bd49587d8c5d66a66018422a36
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-02 16:36:47 +0900

    VSInoutとVSOutputを宣言

--- DirectX12_App/Shaders/SkyboxVS.hlsl
+++ DirectX12_App/Shaders/SkyboxVS.hlsl
@@ -3,3 +3,11 @@ cbuffer SceneConstant : register(b0){
     float4x4 proj;
 };
 
+struct VSInput{
+    float3 position : POSITION;
+};
+
+struct VSOutput{
+    float4 position : SV_POSITION;
+    float3 texCoord : TEXCOORD;
+};

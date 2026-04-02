f7624460ad2f05d37b84367228bb46f1b9becf2b
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-02 16:57:32 +0900

    Create、ReleaseUploadBuffer、GetResourceを宣言

--- DirectX12_App/Engine/Resources/CubeMapTexture.h
+++ DirectX12_App/Engine/Resources/CubeMapTexture.h
@@ -6,7 +6,14 @@
 
 class CubeMapTexture {
 public:
-	
+	bool Create(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, const std::string faceFiles[6], D3D12_CPU_DESCRIPTOR_HANDLE srvHandle);
+
+	void ReleaseUploadBuffer();
+
+	ID3D12Resource* GetResource() const {
+		return m_texture.Get();
+	}
+
 private:
 	Microsoft::WRL::ComPtr<ID3D12Resource> m_texture;
 	Microsoft::WRL::ComPtr<ID3D12Resource> m_uploadBuffer;

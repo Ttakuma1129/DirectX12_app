b5fa74ba7e85ecd025bd6208a52c903d01ac9786
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-02 16:47:18 +0900

    mainを作成

--- DirectX12_App/Shaders/SkyboxPS.hlsl
+++ DirectX12_App/Shaders/SkyboxPS.hlsl
@@ -5,3 +5,7 @@ struct PSInput{
     float4 position : SV_POSITION;
     float3 texCoord : TEXCOORD;
 };
+
+float4 main(PSInput input) : SV_TARGET{
+    return skyTexture.Sample(smp, input.texCoord);
+}

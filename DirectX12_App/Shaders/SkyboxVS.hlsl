62efca169e5852a2e05bb257926d06dbe7a4d6c8
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-02 16:42:32 +0900

    mainを作成

--- DirectX12_App/Shaders/SkyboxVS.hlsl
+++ DirectX12_App/Shaders/SkyboxVS.hlsl
@@ -11,3 +11,22 @@ struct VSOutput{
     float4 position : SV_POSITION;
     float3 texCoord : TEXCOORD;
 };
+
+VSOutput main(VSInput input){
+    VSOutput output;
+    
+    // 視線方向をテクスチャ座標として扱う
+    output.texCoord = input.position;
+    
+    // View行列から平行移動を除外
+    float4x4 viewNoTranslation = view;
+    viewNoTranslation[3] = float4(0, 0, 0, 1);
+    
+    float4 pos = mul(float4(input.position, 1.0), viewNoTranslation);
+    pos = mul(pos, proj);
+    
+    // 常に最奥に表示されるように深度を最大値に固定
+    output.position = pos.xyww;
+    
+    return output;
+}

95bf8e31bb6a17a17652a1b8d7503d44e796c996
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-02 23:21:30 +0900

    Initializeにルートシグネチャ設定を追加

--- DirectX12_App/Engine/Graphics/Skybox.cpp
+++ DirectX12_App/Engine/Graphics/Skybox.cpp
@@ -1,3 +1,16 @@
 #include "Skybox.h"
 #include "CommandContext.h"
 
+bool Skybox::Initialize(ID3D12Device* device, ID3D12CommandQueue* commandQueue, ID3D12CommandAllocator* allocator, const std::string faceFiles[6]) {
+	HRESULT hr;
+
+	// ルートシグネチャ設定
+	D3D12_DESCRIPTOR_RANGE srvRange = {};
+	srvRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	srvRange.NumDescriptors = 1;
+	srvRange.BaseShaderRegister = 0;
+	srvRange.RegisterSpace = 0;
+	srvRange.OffsetInDescriptorsFromTableStart = 0;
+
+	D3D12_ROOT_PARAMETER rootParam[2] = {};
+}

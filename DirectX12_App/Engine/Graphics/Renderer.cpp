946b1d08017c5c0e3dc19baa5b41ad79471c64d6
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-06 12:53:02 +0900

    trueをfalseに変更

--- DirectX12_App/Engine/Graphics/Renderer.cpp
+++ DirectX12_App/Engine/Graphics/Renderer.cpp
@@ -137,7 +137,7 @@ bool Renderer::Initialize(ID3D12Device* device, ID3D12CommandQueue* commandQueue
 	Microsoft::WRL::ComPtr<ID3D12Fence> fence;
 	hr = device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
 	if (FAILED(hr)) {
-		return true;
+		return false;
 	}
 
 	HANDLE event = CreateEvent(nullptr, FALSE, FALSE, nullptr);

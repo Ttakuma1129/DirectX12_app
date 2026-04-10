a7d9faa6929b8813da27008b6010e0eec2cc3497
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-10 16:31:12 +0900

    SceneObjectを移動

--- DirectX12_App/Engine/Resources/SceneObject.h
+++ DirectX12_App/Engine/Resources/SceneObject.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+#include <cstdint>
+
+// オブジェクト1つ分のデータ
+struct SceneObject {
+	std::string modelPath;	// モデルのファイルパス
+	uint32_t meshIndex = 0; // Rendererで使うメッシュ番号
+	float position[3] = { 0,0,0 }; //オブジェクトの位置
+	float rotation[3] = { 0,0,0 }; // オブジェクトの回転(ラジアン)
+	float scale = 1.0f;
+	char name[32] = "Object"; // ImGuiで表示する名前
+};

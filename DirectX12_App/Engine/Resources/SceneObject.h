2cf620ffca370c393af1f70e00b7af0450a0ab36
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-14 13:01:08 +0900

    texturePathを追加

--- DirectX12_App/Engine/Resources/SceneObject.h
+++ DirectX12_App/Engine/Resources/SceneObject.h
@@ -5,6 +5,7 @@
 // オブジェクト1つ分のデータ
 struct SceneObject {
 	std::string modelPath;	// モデルのファイルパス
+	std::string texturePath = "app/Textures/sample.png"; // テクスチャのパス
 	uint32_t meshIndex = 0; // Rendererで使うメッシュ番号
 	float position[3] = { 0,0,0 }; //オブジェクトの位置
 	float rotation[3] = { 0,0,0 }; // オブジェクトの回転(ラジアン)

4267512b24530cd72a9c678dadbb87ed51089466
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-16 23:28:54 +0900

    チャンククラスを作成

--- DirectX12_App/App/World/Chunk.h
+++ DirectX12_App/App/World/Chunk.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <vector>
+
+#include "BlockType.h"
+#include "../../Engine/Resources/ModelLoader.h"
+
+class BlockType;
+
+class Chunk {
+public:
+	void GenerateFlat();
+
+	void SetBlock(int x, int y, int z) const;
+
+	void BuildMesh(std::vector<ModelVertex>& vertices, std::vector<uint16_t>& indices) const;
+
+	// ブロック取得・設定
+	BlockType GetBlock(int x, int y, int z) const;
+
+private:
+	static constexpr int SIZE = 16;
+	static constexpr int HEIGHT = 16;
+
+	BloackType m_blocks[SIZE][HEIGHT][SIZE] = {};
+	
+	bool IsSolid(int x, int y, int z) const;
+	void AddFace(int faceDir, int x, int y, int z, std::vector<ModelVertex>& vertices, std::vector<uint16_t>& indices) const;
+};

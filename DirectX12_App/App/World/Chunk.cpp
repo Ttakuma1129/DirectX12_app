e603c9feb24ad9b81b616b272ec684960633588c
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-17 22:24:58 +0900

    AddFaceに面ごとの法線～4頂点追加を追加

--- DirectX12_App/App/World/Chunk.cpp
+++ DirectX12_App/App/World/Chunk.cpp
@@ -55,8 +55,47 @@ bool Chunk::IsSolid(int x, int y, int z) const {
 void Chunk::AddFace(int faceDir, int x, int y, int z, std::vector<ModelVertex>& vertices, std::vector<uint16_t>& indices) const {
 	// 方向事の頂点位置オフセット(ブロックの原点基準)
 	static const float faceVertices[6][4][3] = {
-		{{1,0,0},{1,0,1},{1,1,1},{1,1,0}},
-		{{0,0,1},{0,0,0},{0,1,0},{0,1,1}},
-		{{0,1,0},{1,1,0},{1,1,1},{0,1,1}},
+		{{1,0,0},{1,0,1},{1,1,1},{1,1,0}}, // 右
+		{{0,0,1},{0,0,0},{0,1,0},{0,1,1}}, // 左
+		{{0,1,0},{1,1,0},{1,1,1},{0,1,1}}, // 上
+		{{0,0,1},{1,0,1},{1,0,0},{0,0,0}}, // 下
+		{{1,0,1},{0,0,1},{0,1,1},{1,1,1}}, // 前
+		{{0,0,0},{1,0,0},{1,1,0},{0,1,0}}, // 後
+	};
+
+	// 面ごとの法線
+	static const float faceNormals[6][3] = {
+		{1,0,0}, {-1,0,0},
+		{0,1,0}, {0,-1,0},
+		{0,0,1}, {0,0,-1},
+	};
+
+	// UV座標
+	static const float faceUVs[4][2] = {
+		{0,1},{1,1},{1,0},{0,0},
+	};
+
+	// 頂点インデックス
+	uint16_t baseIndex = static_cast<uint16_t>(vertices.size());
+
+	// 4頂点追加
+	for (int i = 0;i < 4; ++i) {
+		ModelVertex v = {};
+		v.position[0] = faceVertices[faceDir][i][0] + x;
+		v.position[1] = faceVertices[faceDir][i][1] + y;
+		v.position[2] = faceVertices[faceDir][i][2] + z;
+		v.normal[0] = faceNormals[faceDir][0];
+		v.normal[1] = faceNormals[faceDir][1];
+		v.normal[2] = faceNormals[faceDir][2];
+		v.uv[0] = faceUVs[i][0];
+		v.uv[1] = faceUVs[i][1];
+		vertices.push_back(v);
 	}
+
+	indices.push_back(baseIndex + 0);
+	indices.push_back(baseIndex + 1);
+	indices.push_back(baseIndex + 2);
+	indices.push_back(baseIndex + 0);
+	indices.push_back(baseIndex + 2);
+	indices.push_back(baseIndex + 3);
 }

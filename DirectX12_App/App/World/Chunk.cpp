17ed6c324201705ae77f7a5334b85d0ceaca0c1a
takuma <kss23200080@nsgcl.ocm>
2026-04-17 09:36:17 +0900

    GenerateFlatを作成

--- DirectX12_App/App/World/Chunk.cpp
+++ DirectX12_App/App/World/Chunk.cpp
@@ -1,14 +1,34 @@
 #include "Chunk.h"
 
+void Chunk::GenerateFlat() {
+	for (int x = 0; x < CHUNK_SIZE; ++x) {
+		for (int z = 0; z < CHUNK_SIZE; ++z) {
+			for (int y = 0; y < CHUNK_SIZE; ++y) {
+				BlockType type;
+				if (y == 7) {
+					type = BlockType::Grass;
+				}
+				else if (y >= 5) {
+					type = BlockType::Dirt;
+				}
+				else {
+					type = BlockType::Stone;
+				}
+				SetBlock(x, y, z, type);
+			}
+		}
+	}
+}
+
 void Chunk::SetBlock(int x, int y, int z, BlockType type) {
-	if (x < 0 || x >= SIZE || y < 0 || y >= HEIGHT || z < 0 || z >= SIZE) {
+	if (x < 0 || x >= CHUNK_SIZE || y < 0 || y >= HEIGHT || z < 0 || z >= CHUNK_SIZE) {
 		return;
 	}
 	m_blocks[x][y][z] = type;
 }
 
 BlockType Chunk::GetBlock(int x, int y, int z) const {
-	if (x < 0 || x >= SIZE || y < 0 || y >= HEIGHT || z < 0 || z >= SIZE) {
+	if (x < 0 || x >= CHUNK_SIZE || y < 0 || y >= HEIGHT || z < 0 || z >= CHUNK_SIZE) {
 		return BlockType::Air;
 	}
 	return m_blocks[x][y][z];

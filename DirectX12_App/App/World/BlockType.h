196c75a6f610668e6de8bf450fbe71845f6c8852
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-20 00:12:21 +0900

    BlockTexCoordとkBlockTexを追加

--- DirectX12_App/App/World/BlockType.h
+++ DirectX12_App/App/World/BlockType.h
@@ -7,3 +7,17 @@ enum class BlockType : uint8_t {
 	Grass,
 	Stone,
 };
+
+struct BlockTexCoord{
+	uint8_t top;
+	uint8_t bottom;
+	uint8_t side;
+};
+
+// ブロックごとのテクスチャ定義 (テクスチャアトラスのセル番号)
+static constexpr BlockTexCoord kBlockTex[] = {
+	{0, 0, 0},	// Air
+	{0, 0, 0},	// Dirt
+	{2, 0, 1},	// Grass
+	{3, 3, 3},	// Stone
+};

a0d84f02a851f56f75fc26018fbc89410e2456e9
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-20 09:08:17 +0900

    GetTexIndexを作成

--- DirectX12_App/App/World/BlockTexture.h
+++ DirectX12_App/App/World/BlockTexture.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
 
+#include "BlockType.h"
+
 struct BlockTexCoord {
 	uint8_t top;
 	uint8_t bottom;
@@ -9,8 +11,27 @@ struct BlockTexCoord {
 
 // ブロックごとのテクスチャ定義 (テクスチャアトラスのセル番号)
 static constexpr BlockTexCoord kBlockTex[] = {
+// {上, 下, 横}
 	{0, 0, 0},	// Air
 	{0, 0, 0},	// Dirt
 	{2, 0, 1},	// Grass
 	{3, 3, 3},	// Stone
 };
+
+// ブロックタイプと面方向からテクスチャアトラスのセル番号を返す
+static uint8_t GetTexIndex(BlockType type, int faceDir) {
+	const BlockTexCoord& t = kBlockTex[static_cast<size_t>(type)];
+	switch (faceDir)
+	{
+	case 2:
+		return t.top; // 上
+
+	case3:
+		return t.bottom; // 下
+
+	default:
+		return t.side; // 横
+
+		break;
+	}
+}

59f97f5ab5e14393388e29bba53665452ce36927
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-20 19:10:06 +0900

    ChunkCoord、ChunkCoordHashを追加

--- DirectX12_App/App/World/World.h
+++ DirectX12_App/App/World/World.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <unordered_map>
+#include <memory>
+#include "Chunk.h"
+#include "BlockType.h"
+
+// チャンクごとの座標
+struct ChunkCoord{
+	int x, z;
+	bool operator==(const ChunkCoord& o)const {
+		return x == o.x && z == o.z;
+	}
+};
+
+// unorderd_mapのキーに使うためのハッシュ
+struct ChunkCoordHash {
+	size_t operator() (const ChunkCoord& c) const {
+
+	}
+};
+

bdbd53c5b22889db11d286a93ac765c29bc8e5e1
DESKTOP-FC4S2C5\kuren <kss-23200080@gmail.com>
2026-04-21 22:12:56 +0900

    GetBlockにYが範囲外ならAirにする～ワールド座標をローカル座標に変換を追加

--- DirectX12_App/App/World/World.cpp
+++ DirectX12_App/App/World/World.cpp
@@ -1,9 +1,26 @@
 #include "World.h"
 
 BlockType World::GetBlock(int worldX, int worldY, int worldZ)const {
+	// Yが範囲外ならAirにする
+	if (worldY < 0 || worldY >= Chunk::HEIGHT) {
+		return BlockType::Air;
+	}
 
+	// ワールド座標をチャンク座標に変換
+	int chunkX = WorldCoord::FloorDiv(worldX, Chunk::CHUNK_SIZE);
+	int chunkZ = WorldCoord::FloorDiv(worldZ, Chunk::CHUNK_SIZE);
 
-	return BlockType::Air;
+	// そのブロックが存在しているチャンクを探す
+	auto it = m_chunks.find({ chunkX, chunkZ });
+	if (it == m_chunks.end()) {
+		return BlockType::Air;	// 見つからなかった場合airを返す
+	}
+
+	// ワールド座標をローカル座標に変換
+	int localX = WorldCoord::Mod(worldX, Chunk::CHUNK_SIZE);
+	int localZ = WorldCoord::Mod(worldZ, Chunk::CHUNK_SIZE);
+
+	return it->second->GetBlock(localX, worldY, localZ);
 }
 
 bool World::IsSolid(int worldX, int worldY, int worldZ)const {

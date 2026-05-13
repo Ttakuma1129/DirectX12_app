0247452bf23d2a37d7ae806b9be21cd1d677e697
takuma <kss23200080@nsgcl.ocm>
2026-05-13 12:13:54 +0900

    Frustumクラスを作成

---
+++ DirectX12_App/Engine/Math/Frustum.cpp
@@ -0,0 +1,9 @@
+#include "Frustum.h"
+
+void Frustum::ExtractFormMatrix(const DirectX::XMMATRIX& viewProj) {
+
+}
+
+bool Frustum::IntersectsAABB(const DirectX::XMFLOAT3& mn, const DirectX::XMFLOAT3& mx) const {
+
+}
--- DirectX12_App/Engine/Math/Frustum.h
+++ DirectX12_App/Engine/Math/Frustum.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <DirectXMath.h>
+
+class Frustum {
+public:
+	// VP行列から6面を抽出
+	void ExtractFormMatrix(const DirectX::XMMATRIX& viewProj);
+
+	// AABBがフラスタムの内側または交差しているか
+	bool IntersectsAABB(const DirectX::XMFLOAT3& mn, const DirectX::XMFLOAT3& mx) const;
+
+private:
+	DirectX::XMFLOAT4 m_planes[6];
+};

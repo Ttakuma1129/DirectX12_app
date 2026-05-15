a4d3df59770b5286e4db2a791667b11b5b574977
takuma <kss23200080@nsgcl.ocm>
2026-05-15 10:35:07 +0900

    GetAABB作成

--- DirectX12_App/App/Player/Player.cpp
+++ DirectX12_App/App/Player/Player.cpp
@@ -12,7 +12,10 @@ void Player::Update(float deltaTime, const World& world, bool forward, bool back
 }
 
 void Player::GetAABB(DirectX::XMFLOAT3& mn, DirectX::XMFLOAT3& mx) const {
-
+	mn = { m_position.x, m_position.y, m_position.z };
+	mx = { m_position.x + WIDTH,
+		   m_position.y + HEIGHT,
+		   m_position.z + WIDTH };
 }
 
 bool Player::CollidesWithWorld(const World& world, const DirectX::XMFLOAT3& mn, const DirectX::XMFLOAT3& mx) const {

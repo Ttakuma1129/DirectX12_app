81337f3baead38963a06f42a2d2698d1e5ae8bb2
takuma <kss23200080@nsgcl.ocm>
2026-05-15 10:15:41 +0900

    Playerクラスを作成

---
+++ DirectX12_App/App/Player/Player.cpp
@@ -0,0 +1,21 @@
+#include "Player.h"
+
+void Player::Initialize(const DirectX::XMFLOAT3& spawnPos) {
+
+}
+
+void Player::Update(float deltaTime, const World& world, bool forward, bool backward, bool left, bool right, bool jump, float yaw) {
+
+}
+
+void Player::GetAABB(DirectX::XMFLOAT3& mn, DirectX::XMFLOAT3& mx) const {
+
+}
+
+bool Player::CollidesWithWorld(const World& world, const DirectX::XMFLOAT3& mn, const DirectX::XMFLOAT3& mx) const {
+
+}
+
+void Player::MoveAxis(int axis, float delta, const World& world) {
+
+}
--- DirectX12_App/App/Player/Player.h
+++ DirectX12_App/App/Player/Player.h
@@ -1,1 +1,50 @@
 #pragma once
+#include <DirectXMath.h>
+
+class World;
+
+class Player {
+public:
+	void Initialize(const DirectX::XMFLOAT3& spawnPos);
+	
+	// 入力によって位置と速度を更新
+	void Update(float deltaTime, const World& world, bool forward, bool backward, bool left, bool right, bool jump, float yaw);
+
+	DirectX::XMFLOAT3 GetPosition() const {
+		return m_position;
+	}
+
+	// 目(カメラ)の位置
+	DirectX::XMFLOAT3 GetEyePosition() const {
+		return { m_position.x + 0.5f * WIDTH,
+				 m_position.y + EYE_HEIGHT,
+				 m_position.z + 0.5f * WIDTH };
+	}
+
+	bool IsOnGround() const {
+		return m_onGround;
+	}
+
+private:
+	// プレイヤーのサイズ・物理定数
+	static constexpr float WIDTH = 0.6f;
+	static constexpr float HEIGHT = 1.8f;
+	static constexpr float EYE_HEIGHT = 1.8f;
+	static constexpr float MOVE_SPEED = 5.0f;
+	static constexpr float JUMP_SPEED = 9.0f;
+	static constexpr float GRAVITY = 28.0f;
+
+	DirectX::XMFLOAT3 m_position = { 0, 0, 0 }; // 足元の中心
+	DirectX::XMFLOAT3 m_velocity = { 0, 0, 0 };
+
+	bool m_onGround = false; // 地面に触れているかどうか(trueで地面に触れている)
+
+	// AABBを取得
+	void GetAABB(DirectX::XMFLOAT3& mn, DirectX::XMFLOAT3& mx) const;
+
+	// 指定したワールド領域にブロックがあるかどうか
+	bool CollidesWithWorld(const World& world, const DirectX::XMFLOAT3& mn, const DirectX::XMFLOAT3& mx) const;
+
+	// 軸ごとに移動を試みて衝突したら停止
+	void MoveAxis(int axis, float delta, const World& world);
+};
